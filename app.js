8015c7094de5f490d63def9509ec04cab43df874
KimInteger <kjs20151240@gmail.com>
2024-07-18 23:21:47 +0900

    chore_Integer : add fetch

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+import http from 'http';
+import fs from 'node:fs';
+import fetch from 'node-fetch';
+import qs from 'querystring'
+
 const PORT = 3000;
-const fetch = require('node-fetch')
 
 // 서버 생성
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    fs.readFile('./index.html', (err, data) => {
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
@@ -23,28 +24,42 @@ const server = http.createServer((req, res) => {
     });
     req.on('end', () => {
       console.log('Received data:', body);
+
+      // body를 JSON으로 파싱
+      let parsedData;
+      try {
+        parsedData = qs.parse(body);
+        console.log(parsedData);
+        console.log(JSON.stringify(parsedData));
+      } catch (err) {
+        console.error('Invalid JSON received:', err);
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Bad Request');
+        return;
+      }
+
       // dbServer로 데이터 전송
-      fetch('http://localhost:8000/save',{
-        method : 'POST',
-        headers : {'Content-Type' : 'application/json'},
-        body : JSON.stringify({data : body}),
+      fetch('http://localhost:8000/save', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(parsedData), // 이미 JSON 객체이므로 그대로 전송
       })
-      .then((res)=>{
-        if(!res.ok){
-          throw new Error('response error!');
-        }
-        return res.json();
-      })
-      .then((data)=>{
-        console.log('perfect! fetch!', data);
-        res.writeHead(200,{"Content-Type":"application/json"});
-        res.end(data);
-      })
-      .catch((err)=>{
-        console.error('this is error', err);
-        res.writeHead(500,{'Content-Type':'text/plain'});
-        res.end('server error')
-      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Response error!');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log('Perfect! Fetch!', data);
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(data));
+        })
+        .catch((err) => {
+          console.error('This is error', err);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Server error');
+        });
     });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
